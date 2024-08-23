a32c8a3b8ed701fbf84be27f4d3bcdc7e3432d53
bruno notebook <bruniculos2014@gmail.com>
2024-08-23 18:01:23 -0300

    update

--- trabalho01/client.cpp
+++ trabalho01/client.cpp
@@ -1,13 +1,16 @@
 #include "net_stuff.h"
 using namespace std;
 
-int main(void)
+void *listen(void *arg)
 {
+    int *freq;
+    freq = (int *) arg;
+
     int socket_id = socket(AF_INET, SOCK_DGRAM, 0);
     if(socket_id < 0)
     {
         cout << "\n[-] Could not create socket" << endl;
-        return 1;
+        pthread_exit((void *) 1);
     }
 
     sockaddr_in server_address = {0};
@@ -16,14 +19,40 @@ int main(void)
     server_address.sin_port = htons(port);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(socket_id, (sockaddr *)&server_address, sizeof(server_address)) < 0) {
+    if (bind(socket_id, (sockaddr *)&server_address, sizeof(server_address)) < 0){
         cout << "\n[-] Could not bind socket" << endl;
-        return 1;
+        pthread_exit((void *) 1);
     }
 
     char buffer[1024];
     socklen_t server_len;
     cout << "\n[+] Started listening..." << endl;
-    int msg_len = recvfrom(socket_id, buffer, 1024, 0, (sockaddr *) &server_address,  &server_len);
-    cout << "\n[+] Received: " << buffer << endl;
+    int index = 0;
+    while(true)
+    {
+        int msg_len = recvfrom(socket_id, buffer, 1024, 0, (sockaddr *) &server_address,  &server_len);
+        cout << "\n[+] Received: " << buffer << endl;
+        sscanf(buffer, "%d", &index);
+        freq[index - 1] += 1;
+    }
+    pthread_exit((void *) 0);
+}
+
+int main(void)
+{
+    pthread_t listener;
+    int *freq = new int[10];
+    int rc = pthread_create(&listener, NULL, listen, (void *) freq);
+    sleep(15);
+    rc = pthread_cancel(listener); 
+
+    FILE *fptr;
+    fptr = fopen("histogram.txt", "w");
+    for(int i = 0; i < 10; i++)
+    {
+        fprintf(fptr, "freq(%d) = %d\n", i + 1, freq[i]);
+    }
+    fclose(fptr);
+    delete [] freq;
+    return 0;
 }
--- trabalho01/server.cpp
+++ trabalho01/server.cpp
@@ -5,7 +5,7 @@ char server_dir[LEN];
 char ipv4[16];
 pthread_mutex_t mtx_thread_info;
 
-void sendRandMulticast(unsigned int port)
+void sendMulticast(unsigned int port, char *data)
 {
     sockaddr_in server_address;
     int socket_id = socket(AF_INET, SOCK_DGRAM, 0);
@@ -18,7 +18,6 @@ void sendRandMulticast(unsigned int port)
     const char* host = "224.000.000.001";
     inet_aton(host, &server_address.sin_addr);
     server_address.sin_port = htons(port);
-    const char *data = "SDI";
     sendto(socket_id, data, sizeof(data), 0, (sockaddr *) &server_address, sizeof(server_address));
     close(socket_id);
     cout << "\n[+] Sended some stuff..." << endl;
@@ -26,9 +25,11 @@ void sendRandMulticast(unsigned int port)
 
 int main(void)
 {
+    char buffer[1024];
     for(int i = 0; i < 15; i++)
     {
-        sendRandMulticast(12345);
+        sprintf(buffer, "%d", rand()%10 + 1);
+        sendMulticast(12345, buffer);
         sleep(1);
     }
 }
